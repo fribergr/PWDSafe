59fd433407279e265343ca5f58fbef945000eb81
Robin Friberg <robin.friberg@gmail.com>
2017-03-14 08:16:58 +0100

    Add tests for encryption and decryption with public and private keys

--- tests/EncryptionTest.php
+++ tests/EncryptionTest.php
@@ -16,15 +16,29 @@ class EncryptionTest extends PHPUnit_Framework_TestCase
          */
         private $encryption;
 
+        private $privkey;
+        private $pubkey;
+
         public function setUp()
         {
                 $this->encryption = new Encryption();
+                list($this->privkey, $this->pubkey) = Encryption::genNewKeys();
         }
 
         public function testEncDec()
         {
                 $encoded = $this->encryption->enc("My secret credential", "SomePassword");
+                $this->assertNotEquals("My secret credential", $encoded);
                 $decrypted = $this->encryption->dec($encoded, "SomePassword");
                 $this->assertEquals("My secret credential", $decrypted);
+        }
+
+        public function testEncWithPub()
+        {
+            $data = "Testdata";
+            $enc = $this->encryption->encWithPub($data, $this->pubkey);
+            $this->assertNotEquals($data, $enc);
+            $dec = $this->encryption->decWithPriv($enc, $this->privkey);
+            $this->assertEquals($data, $dec);
         }
 }

62b42fb66c81a04da5df131c21a1ed494d7c5785
Robin Friberg <robin@devpeak.se>
2020-02-18 22:18:23 +0100

    Fix change password layout to conform to field names correctly

--- resources/views/changepassword.blade.php
+++ resources/views/changepassword.blade.php
@@ -1,20 +1,21 @@
 @extends('layouts.master')
 @section('content')
 <div class="container">
-    <form method="post" class="form-horizontal" id="pwdchangeform">
+    <form method="post" class="form-horizontal" action="{{ route('changepassword') }}">
+        @csrf
         <div class="form-group">
             <label for="oldpwd">Old password</label>
             <input type="password" id="oldpwd" name="oldpwd" class="form-control" placeholder="Old password">
         </div>
         <div class="form-group">
-            <label for="newpwd1">New password</label>
-            <input type="password" id="newpwd1" name="newpwd1" class="form-control" placeholder="New password">
+            <label for="password">New password</label>
+            <input type="password" id="password" name="password" class="form-control" placeholder="New password">
         </div>
         <div class="form-group">
-            <label for="newpwd2">Repeat</label>
-            <input type="password" id="newpwd2" name="newpwd2" class="form-control" placeholder="Repeat">
+            <label for="password_confirmation">Repeat</label>
+            <input type="password" id="password_confirmation" name="password_confirmation" class="form-control" placeholder="Repeat">
         </div>
-        <button type="button" id="changePwd" class="btn btn-primary">Change password</button>
+        <button type="submit" class="btn btn-primary">Change password</button>
     </form>
 </div>
 @endsection()

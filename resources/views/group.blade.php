9b7c3e2002605256acf77f8ecb3c8bb47f9c4235
Robin Friberg <robin.friberg@gmail.com>
2020-09-29 18:06:22 +0200

    Avoid include of credentialcard in group view due to performance hit

--- resources/views/group.blade.php
+++ resources/views/group.blade.php
@@ -31,7 +31,30 @@
     @if ($credentials->count() > 0)
     <div class="card-columns">
         @foreach($credentials as $credential)
-            @include('partials._credentialCard', ['credential' => $credential, 'showGroupName' => false])
+            <div class="card my-2">
+                <div class="card-body">
+                    <h5 class="card-title">{{ $credential->site }}</h5>
+                    <h6 class="card-subtitle mb-2 text-muted">{{ $credential->username }}</h6>
+                    <p class="card-text">{{ $credential->notes }}</p>
+                </div>
+                <div class="card-footer">
+                    <div class="clearfix">
+                        <div class="float-left my-1">
+                        </div>
+                        <div class="btn-group float-right">
+                            <button class="btn btn-outline-primary showPass" data-id="{{ $credential->id }}">
+                                <i class="far fa-eye" title="Show"></i>
+                            </button>
+                            <button class="btn btn-outline-secondary copypwd" data-id="{{ $credential->id }}">
+                                <i class="far fa-copy" title="Copy to clipboard"></i>
+                            </button>
+                            <button class="btn btn-outline-danger popconfirm credDelete" data-id="{{ $credential->id }}">
+                                <i class="far fa-trash-alt" title="Delete"></i>
+                            </button>
+                        </div>
+                    </div>
+                </div>
+            </div>
         @endforeach
     </div>
     @else
